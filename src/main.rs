a474a73fd3bcddaca27f0fae8910e4d782c0b86a
Matt Robinson <mthw.wm.robinson@gmail.com>
2024-10-23 08:52:11 -0400

    get response from ollama

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use reqwest;
+use serde_json::json;
+
+#[tokio::main]
+async fn main() {
+    let url = "http://localhost:11434/api/generate";
+
+    let parameters = json!({
+        "model": "llama3.2:1b",
+        "prompt": "Why is the sky blue?",
+        "stream": false,
+
+    });
+
+    let client = reqwest::Client::new();
+    let res = client.post(url).json(&parameters).send().await.unwrap();
+
+    dbg!(res);
 }
